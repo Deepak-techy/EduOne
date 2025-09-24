5ec29c89fcbcdedf75e1cf903dea493d6af0b449
Deepak Ch. Kalita <deepakkalita102@gmail.com>
2025-09-24 13:43:24 +0530

    added frontend project files and configured tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-red-400'>
+        <h1>heloo difuckkkk</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

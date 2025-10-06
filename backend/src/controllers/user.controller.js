aadcf9658c14ea98674846b8c8538f0be56b3cd7
Deepak Ch. Kalita <deepakkalita102@gmail.com>
2025-10-06 19:30:25 +0530

    add user model

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,23 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.send("Registered User")
+    // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, check for avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return response
+
+
+    
 })
 
 

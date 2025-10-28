430bc4a4b7794575776ce58139564834609c6aa1
Deepak Ch. Kalita <deepakkalita102@gmail.com>
2025-10-28 13:09:59 +0530

    add getNotesWithOptionalFilters, getLastUpdatedNotes, getNoteById, updateNote controllers and their respective routes

--- backend/src/routes/note.routes.js
+++ backend/src/routes/note.routes.js
@@ -3,6 +3,10 @@ import { Router } from "express";
 import {
     createNote,
     uploadAndProcessDocument,
+    getNotesWithOptionalFilters,
+    getLastUpdatedNotes,
+    getNoteById,
+    updateNote,
 } from "../controllers/note.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -11,6 +15,10 @@ const router = Router();
 
 router.route("/create").post(verifyJWT, createNote)    // POST /api/notes/create
 router.route("/:noteId/upload").post(verifyJWT, upload.single("pdf"), uploadAndProcessDocument)    // POST /api/notes/:noteId/upload
+router.route("/all").get(verifyJWT, getNotesWithOptionalFilters)    // GET /api/notes/all
+router.route("/last-updated").get(verifyJWT, getLastUpdatedNotes)    // GET /api/notes/last-updated
+router.route("/:noteId").get(verifyJWT, getNoteById)    // GET /api/notes/:noteId
+router.route("/:noteId/update").patch(verifyJWT, updateNote)    // PATCH /api/notes/:noteId/update
 
 
 export default router;
